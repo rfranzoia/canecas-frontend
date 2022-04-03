f2153c4c1fcb900230941ac0416d08c8eb6c2566
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-04-03 21:01:10 +0100

    allow default card to not have a header

--- src/components/ui/DefaultCard.js
+++ src/components/ui/DefaultCard.js
@@ -3,7 +3,9 @@ import {Card} from "react-bootstrap";
 export const DefaultCard = (props) => {
     return (
         <Card border="dark" {...props}>
-            <Card.Header as="h4">{props.title}</Card.Header>
+            {props.title &&
+                <Card.Header as="h4">{props.title}</Card.Header>
+            }
             <Card.Body>{props.children}</Card.Body>
         </Card>
     )
