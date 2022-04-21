80e969e218a1a3f85f6f80ee165c9c0c9d0f216c
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-04-21 21:57:24 +0100

    fixed order row missing fields

--- src/components/orders/OrderItems.tsx
+++ src/components/orders/OrderItems.tsx
@@ -5,7 +5,9 @@ export const OrderItems = (props) => {
         return (
             <tr key={item._id} className="tr-item">
                 <td width="40%">{item.product}</td>
-                <td width="10%" align="left">{item.amount}</td>
+                <td width="10%" align="center">{item.drawings}</td>
+                <td width="10%" align="center">{item.background}</td>
+                <td width="10%" align="right">{item.amount}</td>
                 <td width="20%" align="right">{item.price.toFixed(2)}</td>
                 <td width="20%" align="right">{(item.price*item.amount).toFixed(2)}</td>
             </tr>
@@ -18,7 +20,9 @@ export const OrderItems = (props) => {
             <table width="100%">
                 <thead><tr>
                     <td>Product</td>
-                    <td>Amount</td>
+                    <td align="center">Drawings</td>
+                    <td align="center">Background</td>
+                    <td align="right">Amount</td>
                     <td align="right">Price</td>
                     <td align="right">Total</td>
                 </tr>
