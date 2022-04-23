0185de992a061718e1efae1cee82f78063f20484
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-04-23 21:09:10 +0100

    added new bordered Row with title and required options

---
+++ src/components/ui/BorderedRow.tsx
@@ -0,0 +1,12 @@
+import {Row} from "react-bootstrap";
+import styles from "./borderedRow.module.css";
+
+export const BorderedRow = (props) => {
+    const required = props.required && <span aria-hidden="true" className="required">*</span>
+    return (
+        <Row className={styles["row-border"]}>
+            <div className={styles["row-title"]}><p>{props.title}{required}</p></div>
+            {props.children}
+        </Row>
+    );
+}
