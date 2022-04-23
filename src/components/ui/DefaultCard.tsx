3701f815c679a6c8bed90527664be9482c9b8987
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-04-23 03:52:18 +0100

    changed header size

--- src/components/ui/DefaultCard.tsx
+++ src/components/ui/DefaultCard.tsx
@@ -4,7 +4,7 @@ export const DefaultCard = (props) => {
     return (
         <Card border="dark" {...props}>
             {props.title &&
-                <Card.Header as="h4">{props.title}</Card.Header>
+                <Card.Header as="h3">{props.title}</Card.Header>
             }
             <Card.Body>{props.children}</Card.Body>
         </Card>
