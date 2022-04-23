548cb816c413d64142d10aaa61c0a5e05cc6af7f
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-04-23 03:53:44 +0100

    adjusted buttons for standardization

--- src/components/ui/ConfirmModal.tsx
+++ src/components/ui/ConfirmModal.tsx
@@ -3,14 +3,14 @@ import {CustomButton} from "./CustomButton";
 
 export const ConfirmModal = (props) => {
     return (
-        <Modal show={props.show} onHide={props.handleClose} keyboard={true} centered>
+        <Modal show={props.show} onHide={props.handleClose} keyboard={true} centered >
             <Modal.Header closeButton>
                 <Modal.Title>{props.title}</Modal.Title>
             </Modal.Header>
             <Modal.Body>{!props.hasData?props.message:props.children}</Modal.Body>
             <div className="actions">
-                <CustomButton caption="No" onClick={props.handleClose} type="close"/>
                 <CustomButton caption="Yes" onClick={props.handleConfirm} type="confirm"/>
+                <CustomButton caption="No" onClick={props.handleClose} type="close"/>
             </div>
         </Modal>
     );
