184bb318ebd46bcfeb40361116a227b95d1ff402
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-04-27 23:40:21 +0100

    updated user retrieve with callback function

--- src/components/users/EditUser.tsx
+++ src/components/users/EditUser.tsx
@@ -1,7 +1,7 @@
 import {usersApi} from "../../api/UsersAPI";
 import {EditUserForm} from "./EditUserForm";
-import {useContext, useEffect, useState} from "react";
-import {ApplicationContext} from "../../context/ApplicationContext";
+import {useCallback, useContext, useEffect, useState} from "react";
+import {AlertType, ApplicationContext} from "../../context/ApplicationContext";
 import {StatusCodes} from "http-status-codes";
 
 export const EditUser = (props) => {
@@ -14,18 +14,20 @@ export const EditUser = (props) => {
         phone: "",
         address: "",
     });
+    const {handleAlert} = appCtx;
+    const {op, id} = props;
 
     const handleSaveUser = (user) => {
-        if (props.op === "view") return;
+        if (op === "view") return;
         const save = async (user) => {
             let result;
-            if (props.op === "edit") {
-                result = await usersApi.withToken(appCtx.userData.authToken).update(props.id, user);
-            } else if (props.op === "new") {
+            if (op === "edit") {
+                result = await usersApi.withToken(appCtx.userData.authToken).update(id, user);
+            } else if (op === "new") {
                 result = await usersApi.withToken(appCtx.userData.authToken).create(user);
             }
             if (result.statusCode !== StatusCodes.OK && result.statusCode !== StatusCodes.CREATED) {
-                handleCloseModal(result.data)
+                handleCloseModal(result)
             } else {
                 handleCloseModal();
             }
@@ -37,32 +39,33 @@ export const EditUser = (props) => {
         props.onCloseModal(error);
     };
 
-    useEffect(() => {
-        const getUser = async () => {
-            if (props.op === "edit" || props.op === "view") {
-                const result = await usersApi.withToken(appCtx.userData.authToken).get(props.id);
-                if (result.statusCode !== StatusCodes.OK) {
-                    appCtx.showErrorAlert(result.name, result.description);
-                } else {
-                    setUser(result.data);
-                }
+    const getUser = useCallback(async () => {
+        if (op === "edit" || op === "view") {
+            const result = await usersApi.withToken(appCtx.userData.authToken).get(id);
+            if (result.statusCode !== StatusCodes.OK) {
+                handleAlert(true, AlertType.DANGER, result.name, result.description);
             } else {
-                setUser({
-                    role: "",
-                    name: "",
-                    email: "",
-                    password: "",
-                    phone: "",
-                    address: "",
-                });
+                setUser(result.data);
             }
-        };
+        } else {
+            setUser({
+                role: "",
+                name: "",
+                email: "",
+                password: "",
+                phone: "",
+                address: "",
+            });
+        }
+    }, [id, op, appCtx.userData.authToken, handleAlert])
+
+    useEffect(() => {
         getUser().then(undefined);
-    }, [props.id, props.op, appCtx]);
+    }, [getUser]);
 
     return (
         <>
-            <EditUserForm user={user} op={props.op} onSaveUser={handleSaveUser} onCancel={handleCloseModal} />
+            <EditUserForm user={user} op={op} onSaveUser={handleSaveUser} onCancel={handleCloseModal} />
         </>
     );
 };
