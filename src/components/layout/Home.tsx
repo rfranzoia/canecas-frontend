41f7b40d040fe7579c5b117936926f5ed32d16a2
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-04-27 23:39:31 +0100

    removed unnecessary alert

--- src/components/layout/Home.tsx
+++ src/components/layout/Home.tsx
@@ -1,14 +1,12 @@
 import {useContext} from "react";
 import {ApplicationContext} from "../../context/ApplicationContext";
 import {Presentation} from "../presentation/Presentation";
-import {AlertToast} from "../ui/AlertToast";
 
 export const Home = () => {
     const appCtx = useContext(ApplicationContext);
 
     return (
         <>
-            <AlertToast />
             <div className="container4">
                 {appCtx.isLoggedIn() &&
                     (
