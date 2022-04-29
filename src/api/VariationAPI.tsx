d0bc667dafba20af399a4aba40b3a1e997088b37
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-04-29 23:04:36 +0100

    code formatting

--- src/api/VariationAPI.tsx
+++ src/api/VariationAPI.tsx
@@ -1,11 +1,10 @@
-import axios, {processRequestError} from "./axios";
-import {DefaultAPI} from "./DefaultAPI";
-import {Variation} from "../domain/Variation";
+import axios, { processRequestError } from "./axios";
+import { DefaultAPI } from "./DefaultAPI";
+import { Variation } from "../domain/Variation";
 
 const VARIATIONS_URL = "/productVariations";
 
 export class VariationsAPI extends DefaultAPI {
-
     list = async (currPage: number = 1) => {
         try {
             const res = await axios.get(`${VARIATIONS_URL}?pageSize=${this.pageSize}&pageNumber=${currPage}`);
@@ -13,12 +12,10 @@ export class VariationsAPI extends DefaultAPI {
         } catch (error: any) {
             return processRequestError(error, "variations:list");
         }
-
-    }
+    };
 
     listByFilter = async (currPage: number, filter: string) => {
-
-        const pageFilter = (filter && filter.startsWith("?"))?filter.concat("&"):"?";
+        const pageFilter = filter && filter.startsWith("?") ? filter.concat("&") : "?";
         const url = `${VARIATIONS_URL}/filterBy${pageFilter}pageSize=${this.pageSize}&pageNumber=${currPage}`;
 
         try {
@@ -27,8 +24,7 @@ export class VariationsAPI extends DefaultAPI {
         } catch (error: any) {
             return processRequestError(error, "variation:listByFilter");
         }
-
-    }
+    };
 
     count = async () => {
         try {
@@ -37,8 +33,7 @@ export class VariationsAPI extends DefaultAPI {
         } catch (error: any) {
             return processRequestError(error, "variation:count");
         }
-
-    }
+    };
 
     get = async (id: string) => {
         try {
@@ -47,48 +42,40 @@ export class VariationsAPI extends DefaultAPI {
         } catch (error: any) {
             return processRequestError(error, "variation:get");
         }
-    }
+    };
 
     create = async (variation: Variation) => {
         try {
             const res = await axios.post(VARIATIONS_URL, JSON.stringify(variation), {
-                headers: { "Content-Type": "application/json",
-                    "Authorization": `Bearer ${this.authToken}`
-                }
+                headers: { "Content-Type": "application/json", Authorization: `Bearer ${this.authToken}` },
             });
             return res.data;
         } catch (error: any) {
             return processRequestError(error, "variation:create");
         }
-
-    }
+    };
 
     update = async (id: string, variation: Variation) => {
         try {
             const res = await axios.put(`${VARIATIONS_URL}/${id}`, JSON.stringify(variation), {
-                headers: { "Content-Type": "application/json",
-                    "Authorization": `Bearer ${this.authToken}`
-                }
+                headers: { "Content-Type": "application/json", Authorization: `Bearer ${this.authToken}` },
             });
             return res.data;
         } catch (error: any) {
             return processRequestError(error, "variation:update");
         }
-    }
+    };
 
     delete = async (id: string) => {
         try {
             await axios.delete(`${VARIATIONS_URL}/${id}`, {
-                headers: { "Content-Type": "application/json",
-                    "Authorization": `Bearer ${this.authToken}`
-                }
+                headers: { "Content-Type": "application/json", Authorization: `Bearer ${this.authToken}` },
             });
             return null;
         } catch (error: any) {
             return processRequestError(error, "variation:delete");
         }
-    }
-
+    };
 }
 
 export const variationsApi = new VariationsAPI();
