bb5f945acfd6198f242fbdd4342f6085bebe14e3
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-04-30 21:50:10 +0100

    added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
+import { lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import { ProtectedRoute } from "./api/ProtectedRouter";
 import { Home } from "./components/layout/Home";
 import { Layout } from "./components/layout/Layout";
-import { OrdersMain } from "./components/orders/OrdersMain";
-import { Products } from "./components/products/Products";
-import { Users } from "./components/users/Users";
-import { Variations } from "./components/variations/Variations";
 import { authActions } from "./store/authSlice";
+
+const Users = lazy(() => import("./components/users/Users").then(({ Users }) => ({ default: Users })));
+const Variations = lazy(() => import("./components/variations/Variations").then(({ Variations }) => ({ default: Variations })));
+const Products = lazy(() => import("./components/products/Products").then(({ Products }) => ({ default: Products })));
+const OrdersMain = lazy(() => import("./components/orders/OrdersMain").then(({ OrdersMain }) => ({ default: OrdersMain })));
 
 function App() {
     const dispatch = useDispatch();
     dispatch(authActions.checkLocalStorage());
     return (
         <Layout>
-            <Switch>
-                <Route path="/" exact><Home/></Route>
-                <ProtectedRoute exact path="/products" component={Products}/>
-                <ProtectedRoute exact path="/variations" component={Variations}/>
-                <ProtectedRoute exact path="/users" component={Users}/>
-                <ProtectedRoute exact path="/orders" component={OrdersMain}/>
-            </Switch>
+            <Suspense fallback={"Loading..."}>
+                <Switch>
+                    <Route path="/" exact><Home/></Route>
+                    <ProtectedRoute exact path="/products" component={Products}/>
+                    <ProtectedRoute exact path="/variations" component={Variations}/>
+                    <ProtectedRoute exact path="/users" component={Users}/>
+                    <ProtectedRoute exact path="/orders" component={OrdersMain}/>
+                </Switch>
+            </Suspense>
         </Layout>
     );
 }
