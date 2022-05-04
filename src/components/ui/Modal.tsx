c1971d0f9c2c2561811be83836a6fdda149da242
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-05-05 00:50:34 +0100

    added closing with ESC support

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,11 +1,19 @@
+import { memo, PropsWithChildren, useCallback, useEffect } from "react";
 import ReactDOM from "react-dom";
 import classes from "./modal.module.css";
+
+interface ModalProps {
+    keyboard?: string,
+    size?: string,
+    style?: object,
+    onClose: Function,
+}
 
 const Backdrop = (props) => {
     return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 
-const ModalOverlay = (props) => {
+const ModalOverlay = (props: PropsWithChildren<ModalProps>) => {
     const classSize = "modal".concat(props.size ? `-${props.size}` : "");
 
     return (
@@ -17,7 +25,20 @@ const ModalOverlay = (props) => {
 
 const portalElement = document.getElementById('overlays');
 
-const Modal = (props) => {
+const Modal = (props: PropsWithChildren<ModalProps>) => {
+    const { onClose, keyboard } = props;
+
+    const close = useCallback((e) => {
+        if (e.keyCode === 27) onClose();
+    }, [onClose]);
+
+    useEffect(() => {
+        if (keyboard) {
+            window.addEventListener("keydown", close);
+        }
+        return (() => window.removeEventListener("keydown", close))
+    },[close, keyboard, onClose]);
+
     return (
         <>
             {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
@@ -29,4 +50,4 @@ const Modal = (props) => {
     );
 };
 
-export default Modal;
+export default memo(Modal);
